ae5670c6a7af9015f9a7ea031cfc07522ab20b06
aimanelyas <aimanelyas9@gmail.com>
2023-11-27 23:58:14 +0800

    bring in some function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Nav from "../components/Nav"
 import Header from "../components/Header"
 import Feed from "../components/Feed"
@@ -6,17 +6,68 @@ import Feed from "../components/Feed"
 
 const App = () => {
 
-  const userId = 'f95ede60-d9b9-4137-b59c-a9d78fc77217'
+  const [user, setUser] = useState(null)
+  const [threads, setThreads] = useState(null)
+  const [viewThreadsFeed, setViewThreadsFeed] = useState(true)
+  const [filteredThreads, setFilteredThreads] = useState(null)
 
+  const userId = "e3096bc3-2a51-4300-8b4f-6180b8b3a660"
+
+  const getUser = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/users?user_uuid=${userId}`)
+      const data = await response.json()
+      setUser(data[0])
+    } catch (error) {
+      console.error(error)  
+    }
+  }
   
+  const getThreads = async () => {
+    try {
+      const response = await fetch (`http://localhost:3000/threads?thread_form=${userId}`)
+      const data = await response.json()
+      setThreads(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getThreadsFeed = () => {
+    if (viewThreadsFeed) {
+      const standAloneThread = threads?.filter(thread => thread.reply_to === null)
+      setFilteredThreads(standAloneThread)
+    }
+    if (!viewThreadsFeed) {
+      const replyThreads = threads?.filter(thread => thread.reply_to!== null)
+      setFilteredThreads(replyThreads)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+    getThreads()
+  }, [])
+
+  useEffect(() => {
+    getThreadsFeed()
+  }, [user, threads, viewThreadsFeed])
+
+  console.log(filteredThreads)
 
   return (
-      <div className="app">
-        <Nav />
-        <Header />
+    <>
+      { user && <div className="app">
+        <Nav url={user.instagram_url} />
+        <Header
+        user= {user}
+        viewThreadsFeed={viewThreadsFeed}
+        setViewThreadsFeed={setViewThreadsFeed}
+        />
         <Feed />
         {/* <PopUp /> */}
-      </div>
+      </div> }
+    </>
   )
 }
 
